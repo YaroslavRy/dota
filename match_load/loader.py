2868835d044e94e7f264d357aa5f0348f9a03586
YaroslavRy <ya.ryndyk@gmail.com>
2019-08-20 23:06:05 +0300

    fixes

--- match_load/loader.py
+++ match_load/loader.py
@@ -23,24 +23,33 @@ class MatchLoader():
                                           '(KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'},
                            {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 '
                                           '(KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'},
-                           {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 '
-                                          '(KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}]
+                           ]
         self.STEAM_API_KEY = '28E3FBFDBF38C8BB4F89F08C6CEB2275'
 
-    def get_working_proxy(self, top_n=25):
+    def get_working_proxy(self, top_n=15):
         user_agent = np.random.choice(self.user_agent, size=1)[0]
         proxy_list_site = 'https://free-proxy-list.net/'
+        proxy_list_site2 = 'https://hidemy.name/en/proxy-list/'
         pm = urllib3.PoolManager(1, headers=user_agent, cert_reqs='CERT_REQUIRED')
         prx_req = pm.request('GET', proxy_list_site)
+        prx_req2 = pm.request('GET', proxy_list_site2)
         prx_soup = BeautifulSoup(prx_req.data, 'html.parser')
+        prx_soup2 = BeautifulSoup(prx_req2.data, 'html.parser')
         proxies_list = []
+
         for tr in prx_soup.find_all('tr')[:top_n]:
             tmp = []
             for td in tr.find_all('td')[:2]:
                 tmp.append(td.text)
             if len(tmp) < 2:
                 continue
-            proxies_list.append([str(tmp[0]) + ':' + tmp[1]])
+            cur_prx = str(tmp[0]) + ':' + tmp[1]
+            proxies_list.append(cur_prx)
+
+        for i, elem in enumerate(prx_soup2.find_all(class_='d')[:top_n]):
+            addr, port = [x.text for x in elem.find_all('td')[:2]]
+            cur_full_prx = addr + ':' + port
+            proxies_list.append(cur_full_prx)
 
         proxies_list = proxies_list[:top_n]
         np.random.shuffle(proxies_list)
@@ -48,12 +57,12 @@ class MatchLoader():
         for i, prx in enumerate(proxies_list):
             try:
                 print(i)
-                cur_prx_address = 'https://' + str(prx[0]) + '/'
+                cur_prx_address = 'https://' + str(prx) + '/'
                 prx_http = urllib3.ProxyManager(cur_prx_address,
                                                 maxsize=1,
                                                 headers=user_agent,
                                                 cert_reqs='CERT_REQUIRED')
-                r = prx_http.request('GET', 'https://stackoverflow.com/', timeout=0.7)
+                r = prx_http.request('GET', 'https://stackoverflow.com/', timeout=0.7, )
                 if r.status == 200:
                     print('Proxy found.')
                     return cur_prx_address
@@ -72,7 +81,6 @@ class MatchLoader():
         return matches
 
     def update_ids(self, last_id, n_first_pages):
-        old_ids = [x['match_id'] for x in self.pro_matches_id.find({}, {'match_id'})]
         cur_prx = None
         while cur_prx is None:
             cur_prx = self.get_working_proxy()
@@ -83,21 +91,20 @@ class MatchLoader():
             print(i, total_matches, last_id)
             try:
                 cur_matches = self.api_pro_matches_id(cur_prx, start_id=last_id)
-                if 'error' in cur_matches and cur_matches['error'] == 'rate limit exceeded':
-                    while cur_prx is None:
-                        cur_prx = self.get_working_proxy()
+                last_id = int(np.min([x['match_id'] for x in cur_matches if 'match_id' in x]))
+                if 'error' in cur_matches:
+                    if cur_matches['error'] == 'rate limit exceeded':
+                        while cur_prx is None:
+                            cur_prx = self.get_working_proxy()
+                        continue
                     continue
-                ids = [x['match_id'] for x in cur_matches if 'match_id' in x]
-                duplicate_ids = np.intersect1d(ids, old_ids)
-                cur_matches = [cur_matches[x['match_id']] for x in cur_matches if
-                               x['match_id'] not in duplicate_ids]
                 if len(cur_matches) > 0:
-                    self.pro_matches_id.insert(cur_matches)
-                    last_id = np.min(ids)
-                print('skipped')
-                last_id = np.min(duplicate_ids)
-            except DuplicateKeyError as de:
-                continue
+                    for match in cur_matches:
+                        try:
+                            self.pro_matches_id.insert(match)
+                            last_id = match['match_id']
+                        except DuplicateKeyError as de:
+                            continue
             except JSONDecodeError as jsnerr:
                 cur_prx = None
                 while cur_prx is None:
@@ -105,20 +112,14 @@ class MatchLoader():
                 continue
             except Exception as err:
                 print('update_ids: \n', err, sep='')
-                # print(err.args)
-                if 'timed out' in str(err.__context__):
-                    print('timed out')
-                    cur_prx = None
-                    while cur_prx is None:
-                        cur_prx = self.get_working_proxy()
-                    if cur_prx is None:
-                        time.sleep(10)
-                continue
+                cur_prx = None
+                while cur_prx is None:
+                    cur_prx = self.get_working_proxy()
             time.sleep(0.1)
 
     def api_get_match(self, prx_address, match_id):
         url = 'https://api.opendota.com/api/matches/'
-        user_agent = self.user_agent
+        user_agent = np.random.choice(self.user_agent, size=1)[0]
         cur_url = url + str(match_id)
         prx_m = urllib3.ProxyManager(prx_address, headers=user_agent, cert_reqs='CERT_REQUIRED')
         r = prx_m.request('GET', cur_url, timeout=2)
@@ -129,7 +130,9 @@ class MatchLoader():
         ids_all = [x['match_id'] for x in self.pro_matches_id.find({}, {'match_id'})]
         ids_stored = [x['match_id'] for x in self.pro_matches.find({}, {'match_id'})]
         need_to_load_ids_bool = np.isin(ids_all, ids_stored, invert=True)
-        need_to_load_ids = np.array(ids_all)[need_to_load_ids_bool]
+        need_to_load_ids = np.array(
+
+        )[need_to_load_ids_bool]
         return need_to_load_ids
 
     def load_insert(self, matches_ids):
@@ -146,12 +149,13 @@ class MatchLoader():
                     print('havent match_id')
                     continue
                 self.pro_matches.insert_one(cur_match)
-            except Exception as e:
-                if 'timeout' in str(e.__context__):
-                    prx_address = self.get_working_proxy()
-                print(e, cur_match_id)
+            except DuplicateKeyError as de:
+                print('DuplicateKeyError')
                 continue
-            time.sleep(0.3)
+            except Exception as e:
+                prx_address = self.get_working_proxy()
+                print('load_insert \n', e, cur_match_id)
+            time.sleep(0.1)
 
     def load_new_matches(self, n_batches):
         new_ids = self.get_new_ids()
@@ -172,5 +176,5 @@ class MatchLoader():
 
 
 loader = MatchLoader()
-loader.update_ids(last_id=99999999999, n_first_pages=1000)
+loader.update_ids(last_id=99999999999, n_first_pages=1)
 loader.load_new_matches(n_batches=8)
